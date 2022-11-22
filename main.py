6d33efa9468245c851db2edd396c3927cf96408b
Alejandro Delgado Ontana <alejandro@Alejandros-MacBook-Pro.local>
2022-11-22 16:29:32 +0100

    making-display-window

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
+import tkinter.ttk
+
+# MAIN
+def main():
+    # Ventana
+    root = Tk()
+    root.title("METRO ATENAS")
+    # Dimensiones de la imagen de fondo (mapa.png)
+    root.geometry("1000x1000")
+    root.resizable(True, True)
+    
+    root.mainloop()
+    
+if __name__ == '__main__':
+    main()
